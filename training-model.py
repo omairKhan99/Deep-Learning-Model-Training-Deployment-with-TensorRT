5247893ac515aa7e8b1593023c52dc0268313264
Omair Khan <omairtkhan@gmail.com>
2025-01-29 23:32:41 -0700

    detecting object

--- training-model.py
+++ training-model.py
@@ -10,3 +10,14 @@ import time
 import onnx
 import onnxruntime as ort
 
+class SimpleCNN(nn.Module): # I am just defining a simple CNN for object detection
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2, 2)
