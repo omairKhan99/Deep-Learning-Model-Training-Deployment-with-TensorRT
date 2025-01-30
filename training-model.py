f489b5cb9db6b5c557a3ef98ca5bd66fb003c76e
Omair Khan <omairtkhan@gmail.com>
2025-01-29 23:35:51 -0700

    new

--- training-model.py
+++ training-model.py
@@ -21,3 +21,13 @@ class SimpleCNN(nn.Module): # I am just defining a simple CNN for object detecti
         self.fc3 = nn.Linear(128, 10)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        x = x.view(x.size(0), -1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
